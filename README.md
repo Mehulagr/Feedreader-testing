bbcf6af0c79fa31f39e3206da0ad5be689f213ab
Mehul Agrawal <agrawal.mehul92@gmail.com>
2019-02-09 22:20:03 -0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Feed Reader by Mehul Agrawal
+
+## Table of Contents
+
+- [Objective](#objective)
+- [Dependencies](#dependencies)
+- [Testing](#testing)
+- [Contributing](#contributing)
+
+## Objective
+The project generates a RSS Feed from multiple sources.
+
+## Dependencies
+JS: app.js, [jsapi](http://google.com/jsapi), [Jquery](https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js), [Handlebar](https://cdnjs.cloudflare.com/ajax/libs/handlebars.js/4.0.12/handlebars.min.js)
+CSS: icomoon.css, normalize.css, style.css
+Font: Roboto
+
+## Testing
+For Testing install: [Jasmine 3.3.0](http://jasmine.github.io/)
+Testing Cases: feedreader.js
+
+## Contributing
+Based on starter code from [Udacity](https://github.com/udacity/frontend-nanodegree-feedreader)
---
+++ jasmine/spec/feedreader.js
@@ -0,0 +1,88 @@
+/* feedreader.js
+ *
+ * This is the spec file that Jasmine will read and contains
+ * all of the tests that will be run against your application.
+ */
+
+/* We're placing all of our tests within the $() function,
+ * since some of these tests may require DOM elements. We want
+ * to ensure they don't run until the DOM is ready.
+ */
+$(function() {
+    /* Testing RSS Feeds (allFeeds array) */
+    describe('RSS Feeds', function() {
+        /* Make sure allFeeds array is defined and not empty */
+        it('are defined', function() {
+            expect(allFeeds).toBeDefined();
+            expect(allFeeds.length).not.toBe(0);
+        });
+
+
+         /* Make sure that feed has a URL */
+         it('have a URL attached to each feed', function() {
+             allFeeds.forEach(function(feed) {
+                 expect(feed.url).toBeDefined();
+                 expect(feed.url).not.toBe('');
+             });
+         });
+
+         /* Make sure that feed has a name */
+         it('have a name', function() {
+           allFeeds.forEach(function(feed) {
+               expect(feed.name).toBeDefined();
+               expect(feed.name).not.toBe('');
+           });
+         });
+    });
+
+    /* Testing the menu */
+    describe('The menu', function() {
+
+      /* The side menu is hidden by default */
+      it('is hidden by default', function() {
+          var selecting_body = document.querySelector('body');
+          expect(selecting_body).toHaveClass('menu-hidden');
+      });
+
+      /* test that toggle for menu is working properly */
+      it('is getting toggled properly', function() {
+          var selecting_body = document.querySelector('body');
+          expect(selecting_body).toHaveClass('menu-hidden');
+
+          document.querySelector('.menu-icon-link').click();
+          expect(selecting_body).not.toHaveClass('menu-hidden');
+
+          document.querySelector('.menu-icon-link').click();
+       });
+    });
+
+    /* Testing out that feed is loaded with something to begin with */
+    describe('Initial Entries', function() {
+         beforeEach(function(done) {
+           loadFeed(0, done);
+         });
+
+         it('are loaded properly', function() {
+           var select_feed = document.querySelector('.feed');
+           expect(select_feed.children.length).toBeGreaterThan(0);
+         });
+     });
+
+    /* Test to see that feed is updating properly */
+    describe('New Feed Selection', function() {
+        var feed = document.querySelector('.feed');
+        var initial_feed, final_feed;
+
+        /* Checking that feed is updating after each call of loadFeed function */
+        beforeEach(function(done) {
+            loadFeed(1);
+            initial_feed = feed.children[0].href;
+            loadFeed(2, done);
+        });
+
+        it('are loaded properly', function() {
+            final_feed = feed.children[0].href;
+            expect(initial_feed).not.toBe(final_feed);
+        });
+    });
+}());
